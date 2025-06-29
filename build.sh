cd467812c5c6c6279958baf341d37f6482fb117d
Bhardwajvivek <87933235+Bhardwajvivek@users.noreply.github.com>
2025-06-29 23:29:30 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# build.sh
-
-# Upgrade pip and install build tools first
-pip install --upgrade pip setuptools wheel
-
-# Install your requirements
-pip install -r requirements.txt
